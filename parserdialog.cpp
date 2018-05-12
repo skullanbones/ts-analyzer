f3c61f43a6f5659a0197ba67a80d290c0efacfad
Christian Köhnenkamp <cvk5@me.com>
2018-05-12 09:17:42 +0200

    [parser] Add PAT parsing callback. 1st version.

--- parserdialog.cpp
+++ parserdialog.cpp
@@ -14,6 +14,7 @@ ParserDialog::ParserDialog(QHexEdit *hexEdit, QWidget *parent) :
     _ui(new Ui::Dialog)
 {
   _ui->setupUi(this);
+  init();
   _hexEdit = hexEdit;
 }
 
@@ -28,7 +29,21 @@ ParserDialog::~ParserDialog()
 void ParserDialog::on_parseButton_clicked()
 {
     qDebug() << "parseButton clicked->start parsing.";
+    parseData();
+    parsePat();
+}
 
+/*****************************************************************************/
+/* Private Methods */
+/*****************************************************************************/
+void ParserDialog::init()
+{
+    _textBrowser = _ui->textBrowser;
+}
+
+void ParserDialog::parseData()
+{
+    printData("Hello World!");
     // Start parsing
     QByteArray data = _hexEdit->data();
     if (data.size()) {
@@ -36,7 +51,7 @@ void ParserDialog::on_parseButton_clicked()
 
         if (data.at(0) != 0x47)
         {
-            qDebug() << "ERROR: 1st byte not in sync!!!";
+            qDebug() << "ERROR: 1'st byte not in sync!!!";
         }
 
         // Start parsing 1st packet data
@@ -44,7 +59,49 @@ void ParserDialog::on_parseButton_clicked()
         TsParser parser;
         parser.parseTsPacketInfo((const uint8_t*)data.data(), info);
         std::cout << info << std::endl;
+        std::stringstream buffer;
+        buffer << info << std::endl;
+        printData(buffer.str());
     }
-
 }
 
+void ParserDialog::printData(std::string inStr)
+{
+    QString str = QString::fromUtf8(inStr.c_str());
+    _ui->textBrowser->setText(str);
+}
+
+void ParserDialog::PATCallback(PsiTable* table)
+{
+    auto pat = dynamic_cast<PatTable*>(table);
+
+    // Print out data
+    std::stringstream buffer;
+    buffer << *pat << std::endl;
+    std::string inStr = buffer.str();
+    QString str = QString::fromUtf8(inStr.c_str());
+    qDebug() << str;
+}
+
+void ParserDialog::parsePat()
+{
+    QByteArray data = _hexEdit->data();
+    uint64_t count = 0;
+    int readIndex = 0;
+    const uint8_t* packetsData = (const uint8_t*)data.data();
+    // Register callback
+    g_tsDemux.addPsiPid(0, std::bind(&ParserDialog::PATCallback, std::placeholders::_1));
+
+    if ((data.at(0) != 0x47) || (data.size() <= 0))
+    {
+        qDebug() << "ERROR: 1'st byte not in sync!!!";
+        return;
+    }
+
+    while (readIndex < data.size())
+    {
+        g_tsDemux.demux(packetsData + readIndex);
+        readIndex += TS_PACKET_SIZE;
+        count++;
+    }
+}
